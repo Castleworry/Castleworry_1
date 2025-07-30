9f6862bfd1a5b5dda23e38e791e5bfea4b6477b1
張宇杰 (Castleworry) <rickychang0111@gmail.com>
2025-07-30 03:39:30 +0000

    2

--- lesson7/lesson7_2.py
+++ lesson7/lesson7_2.py
@@ -3,5 +3,6 @@ import argparse
 parser = argparse.ArgumentParser(description="要求使用者輸入姓名")
 parser.add_argument("name" , help="姓名")
 args = parser.parse_args()
+print(type(args))
 
 print(f"您的姓名是:{args.name}")
---
+++ lesson7/lesson7_4.py
@@ -0,0 +1,15 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="請輸入姓名")
+parser.add_argument("-n","--name",type=str,help="姓名")
+args = parser.parse_args()
+
+if not args.name:
+    name = input("請輸入您的姓名:")
+     
+else:
+   name = args.name
+
+print(name)
+
+
